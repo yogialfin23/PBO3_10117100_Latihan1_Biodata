86dc40e838d2218986e7ef07809d088f180c903d
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-09-23 18:38:18 +0700

    Add files via upload

---
+++ PBO3_10117100_Latihan1_Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117100_latihan1_biodata;
+
+/**
+ *Yogie Alfin Salim
+ *10117100
+ *IF-3 
+ */
+public class PBO3_10117100_Latihan1_Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------");
+        System.out.println("NAMA\t\t: Yogie Alfin Salim");
+        System.out.println("ALAMAT\t\t: Jalan Villa Cibubur Blok T1 no.22");
+        System.out.println("TEMPAT LAHIR\t: Jakarta");
+        System.out.println("TANGGAL LAHIR\t: 23 April 1999");
+        System.out.println("---------------");
+        System.out.println("HOBI\t\t: Hikking & Gaming");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Es Kelapa");
+    }
+    
+}
